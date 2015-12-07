885198acb81635b88bc8123a8687c10b3832c7d0
Heru Joko <heru.mikami@gmail.com>
2015-12-07 18:12:35 +0800

    admin page

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+	
+	class AdminController extends BaseController {
+
+		public function dashboard(){
+			$data['active'] = 'dashboard'; 
+			return View::make('admin.dashboard' , $data);
+		}
+
+	}
+
+?>
