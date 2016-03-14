5d5603106abba178fdf13caa23387b5b1f3bbcef
Heru Joko <heru.mikami@gmail.com>
2016-03-14 10:28:10 +0800

    no message

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'tselno',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'Riset123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
